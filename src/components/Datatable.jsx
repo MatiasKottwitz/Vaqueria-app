700b1c6885478fba4653cd8d353a3597db679b32
MatiasKottwitz <kottvitz.matias@gmail.com>
2022-04-26 00:32:30 -0300

    Se agregaron nuevos campos al modal Nuevo CLiente

--- src/components/Datatable.jsx
+++ src/components/Datatable.jsx
@@ -2,7 +2,7 @@ import React,{useState,useEffect }from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 import Swal from "sweetalert2";
-import { Modal, Button,Form} from "react-bootstrap";
+import { Modal, Button, Form, Col, Row,} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import { Link, NavLink } from "react-router-dom";
 //-------------------------------------------------- Funcion buscar.
@@ -309,33 +309,49 @@ const eliminarCliente =()=>{
       </Modal>
 
 
-      <Modal show={showNuevo} fullscreen={true} onHide={handleClose}>
+      <Modal show={showNuevo} size='lg'  onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title> Nuevo Cliente</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
-        <Form.Group className="mb-3">
-                    <Form.Control 
-                        type="text"
-                        name="razon_social"
-                        placeholder="Razon Social"
-                        value={clienteSeleccionado.razon_social}
-                        onChange={handleChange}
-                        required
-                    />
+        <Form >
+          <Row>
+              <Form.Group className="mb-3" as={Col} lg='3'>
+              <Form.Label>Razon Social</Form.Label>
+                <Form.Control 
+                    type="text"
+                    name="razon_social"
+                    placeholder="Nombre Y Apellido"
+                    value={clienteSeleccionado.razon_social}
+                    onChange={handleChange}
+                    required
+                />
+             </Form.Group>
+             <Form.Group className="mb-3" as={Col} sm='3'>
+                    <Form.Label>Tipo de Documento</Form.Label>
+                     <Form.Select name="tipo_documento" value={clienteSeleccionado.tipo_documento} onChange={handleChange}>
+                      <option >Elija Tipo</option>
+                      <option value="DNI">D.N.I.- Documento Nacional de Identidad.</option>
+                      <option value="LC">L.C.- Libreta Cívica.</option>
+                      <option value="LE">L.E.- Libreta de Enrolamiento.</option>
+                      <option value="C.I">C.I.- Cédula de Identidad.</option>
+                      
+                    </Form.Select>
                 </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Control 
-                        type="number"
-                        name="documento"
-                        placeholder="Documento"
-                        value={clienteSeleccionado.documento}
-                        onChange={handleChange}
-                        required
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3">
+            <Form.Group className="mb-3" as={Col} sm='3'>
+            <Form.Label>Numero Documento</Form.Label>
+              <Form.Control 
+                  type="number"
+                  name="documento"
+                  placeholder="Documento"
+                  value={clienteSeleccionado.documento}
+                  onChange={handleChange}
+                  required
+              />
+          </Form.Group>
+           
+                <Form.Group className="mb-3" as={Col} sm='3'>
+                <Form.Label>Direccion Domicilio</Form.Label>
                     <Form.Control 
                         type="text"
                         name="domicilio"
@@ -345,7 +361,10 @@ const eliminarCliente =()=>{
                         required
                     />
                 </Form.Group>
-                <Form.Group className="mb-3">
+                </Row>
+           <Row> 
+                <Form.Group className="mb-3" as={Col} sm='3'>
+                <Form.Label>Localidad</Form.Label>
                     <Form.Control 
                         type="text"
                         name="localidad"
@@ -355,7 +374,20 @@ const eliminarCliente =()=>{
                         required
                     />
                 </Form.Group>
-                <Form.Group className="mb-3">
+                <Form.Group className="mb-3" as={Col} sm='3'>
+                <Form.Label>Postal</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        name="postal"
+                        placeholder="Codigo Postal"
+                        value={clienteSeleccionado.postal}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" as={Col} sm='3'>
+                <Form.Label>Numero de Telefono</Form.Label>
                     <Form.Control 
                         type="number"
                         name="telefono"
@@ -365,17 +397,21 @@ const eliminarCliente =()=>{
                         required
                     />
                 </Form.Group>
-                <Form.Group className="mb-3">
+                <Form.Group className="mb-3" as={Col}sm="3">
+                <Form.Label>Elija Categoria de IVA</Form.Label>
                      <Form.Select name="categoria_iva" value={clienteSeleccionado.categoria_iva} onChange={handleChange}>
                       <option >Seleccione Categoria</option>
-                      <option value="1"> (1) Régimen Simplificado (RS) - Monotributista</option>
+                      <option value="1"> (1) Monotributista</option>
                       <option value="2">(2) Responsable Inscripto (RI)</option>
                       <option value="3"> (3) Exento</option>
                       <option value="4"> (4) Consumidor Final</option>
                       
                     </Form.Select>
                 </Form.Group>
-                <Form.Group className="mb-3">
+                </Row>
+                <Row>
+                <Form.Group className="mb-3" as={Col} sm="3">
+                <Form.Label>Numero de Cuit</Form.Label>
                     <Form.Control 
                         type="number"
                         name="cuit"
@@ -385,6 +421,41 @@ const eliminarCliente =()=>{
                         required
                     />
                 </Form.Group>
+                
+                <Form.Group className="mb-3" as={Col}sm="3">
+                <Form.Label>Elija Provincia</Form.Label>
+                     <Form.Select name="provincia" value={clienteSeleccionado.provincia} onChange={handleChange}>
+                      <option >Seleccione Provincia </option>
+                      <option value="1"> (1)</option>
+                      <option value="2">(2)</option>
+                      <option value="3"> (3)</option>
+                      <option value="4"> (4) </option>
+                      
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" as={Col}sm="3">
+                <Form.Label>Categoria Ingr. Brutos</Form.Label>
+                     <Form.Select name="categoria_ingresos_brutos" value={clienteSeleccionado.categoria_ingresos_bruto} onChange={handleChange}>
+                      <option >Seleccione Categoria </option>
+                      <option value="1"> (1)</option>
+                      <option value="2">(2)</option>
+                      <option value="3"> (3)</option>
+                      <option value="4"> (4) </option>
+                      
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" as={Col} sm="3">
+                <Form.Label>Inscripcion Ingr. Brutos</Form.Label>
+                    <Form.Control 
+                        type="number"
+                        name="inscripcion_ingresos_brutos"
+                        placeholder="Numero Inscripcion"
+                        value={clienteSeleccionado.inscripcion_ingresos_brutos}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+                </Row>
                 <Modal.Footer>
           <Button variant="success" onClick={()=>cargarCliente()}>
             Guardar
